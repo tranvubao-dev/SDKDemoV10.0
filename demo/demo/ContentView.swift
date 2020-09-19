8d79acf4e218ba912aa3103f6d972aeaad26bb6f
tranvubao <tranvubao.vubao@gmail.com>
2020-09-19 10:17:14 +0700

    addfile

---
+++ demo/demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by BaoTV1 on 9/19/20.
+//  Copyright © 2020 BaoTV1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
